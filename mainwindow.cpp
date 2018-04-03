cb41c982a4b665073f3b834254e9ddb18c80fdaf
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-04-04 01:00:12 +0300

    Added device refresh rates and sensivities

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,29 +8,67 @@
 #include <string>
 #include <QtWidgets/QMessageBox>
 
+#define RIVAL100SENS { "250", "500", "1000", "1250", "1500", "1750", "2000", "4000" }
+
+#define RIVAL110SENS { "200", "300", "400", "500",\
+"600", "700", "800", "900", "1000", "1100", "1200", "1300", "1400",\
+"1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300",\
+"2400", "2500", "2600", "2700", "2800", "2900", "3000", "3100", "3200",\
+"3300", "3400", "3500", "3600", "3700", "3800", "3900", "4000", "4100",\
+"4200", "4300", "4400", "4500", "4600", "4700", "4800", "4900", "5000",\
+"5100", "5200", "5300", "5400", "5500", "5600", "5700", "5800", "5900",\
+"6000", "6100", "6200", "6300", "6400", "6500", "6600", "6700", "6800",\
+"6900", "7000", "7100", "7200" }
+
+#define RIVAL300SENS { "50", "100", "150", "200", "250", "300", "350",\
+"400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900", "950",\
+"1000", "1050", "1100", "1150", "1200", "1250", "1300", "1350", "1400", "1450",\
+"1500", "1550", "1600", "1650", "1700", "1750", "1800", "1850", "1900", "1950",\
+"2000", "2050", "2100", "2150", "2200", "2250", "2300", "2350", "2400", "2450",\
+"2500", "2550", "2600", "2650", "2700", "2750", "2800", "2850", "2900", "2950",\
+"3000", "3050", "3100", "3150", "3200", "3250", "3300", "3350", "3400", "3450",\
+"3500", "3550", "3600", "3650", "3700", "3750", "3800", "3850", "3900", "3950",\
+"4000", "4050", "4100", "4150", "4200", "4250", "4300", "4350", "4400", "4450",\
+"4500", "4550", "4600", "4650", "4700", "4750", "4800", "4850", "4900", "4950",\
+"5000", "5050", "5100", "5150", "5200", "5250", "5300", "5350", "5400", "5450",\
+"5500", "5550", "5600", "5650", "5700", "5750", "5800", "5850", "5900", "5950",\
+"6000", "6050", "6100", "6150", "6200", "6250", "6300", "6350", "6400", "6450" }
+
+#define RIVAL310SENS { "100", "200", "300", "400", "500", "600", "700",\
+"800", "900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700",\
+"1800", "1900", "2000", "2100", "2200", "2300", "2400", "2500", "2600", "2700",\
+"2800", "2900", "3000", "3100", "3200", "3300", "3400", "3500", "3600", "3700",\
+"3800", "3900", "4000", "4100", "4200", "4300", "4400", "4500", "4600", "4700",\
+"4800", "4900", "5000", "5100", "5200", "5300", "5400", "5500", "5600", "5700",\
+"5800", "5900", "6000", "6100", "6200", "6300", "6400", "6500", "6600", "6700",\
+"6800", "6900", "7000", "7100", "7200", "7300", "7400", "7500", "7600", "7700",\
+"7800", "7900", "8000", "8100", "8200", "8300", "8400", "8500", "8600", "8700",\
+"8800", "8900", "9000", "9100", "9200", "9300", "9400", "9500", "9600", "9700",\
+"9800", "9900", "10000", "10100", "10200", "10300", "10400", "10500", "10600",\
+"10700", "10800", "10900", "11000", "11100", "11200", "11300", "11400", "11500",\
+"11600", "11700", "11800", "11900", "12000" }
+
+#define POLLINGRATES { "125", "250", "500", "1000" }
+
 
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString device;
 
     QStringList allDeviceIds = runCommand("xinput --list --id-only").split("\n");
     foreach( QString id, allDeviceIds)
         {
-            QString result3;
-            QStringList result2 = runCommand("xinput --list-props " + id).split("'");
-            if (result2.size() > 2){
-                result3 = result2[1].toLatin1().data();
-            }
-
-            std::string result = runCommand("xinput --list-props "+ id).toLatin1().data();
-            std::string str2 ("SteelSeries Rival");
-
-            // different member versions of find in the same order as above:
-            std::size_t found = result.find(str2);
-            if (found!=std::string::npos) {
-                ui->deviceNameLabel->setText(result3);
+            QString result;
+            QStringList resultList = runCommand("xinput --list-props " + id).split("'");
+            if (resultList.size() > 2){
+                result = resultList[1].toLatin1().data();
+                if (result.contains("SteelSeries Rival")) {
+                    ui->deviceNameLabel->setText(result);
+                    device = result;
+                }
             }
         }
 
@@ -41,11 +79,25 @@ MainWindow::MainWindow(QWidget *parent) :
                 tr("RivalQt"),
                 tr("Please run this command 'sudo pip install rivalcfg' ") );
         exit(1);
-    } else {
-        // Command does exist
-        std::cout << "hede" << endl;
     }
 
+    if (device.contains("100")) {
+        ui->sens1Combo->addItems(RIVAL100SENS);
+        ui->sens2Combo->addItems(RIVAL100SENS);
+    } else if (device.contains("110")) {
+        ui->sens1Combo->addItems(RIVAL110SENS);
+        ui->sens2Combo->addItems(RIVAL110SENS);
+    } else if (device.contains("300")) {
+        ui->sens1Combo->addItems(RIVAL300SENS);
+        ui->sens2Combo->addItems(RIVAL300SENS);
+    } else if (device.contains("310")) {
+        ui->sens1Combo->addItems(RIVAL310SENS);
+        ui->sens2Combo->addItems(RIVAL310SENS);
+    } else {
+        printf("Device not found");
+    }
+
+    ui->pollingRateCombo->addItems(POLLINGRATES);
 
 
 
